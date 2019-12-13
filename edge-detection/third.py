fe1d36a392e04c8e8f739c5eb793872486232399
Ahmed Khaled <ahmed_khaled3cs@ci.suez.edu.eg>
2019-12-14 01:02:00 +0200

    Add files via upload

---
+++ edge-detection/third.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'thirdPage.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5 import QtCore, QtGui, QtWidgets
+import second
+
+class Ui_thirdPageWindow(object):
+
+    
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 633)
+        MainWindow.setStyleSheet("background-color: rgb(41, 40, 38);")
+
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(110, 250, 141, 31))
+
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(480, 250, 231, 31))
+
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(500, 540, 201, 41))
+
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("background-color: rgb(25, 148, 200);")
+        self.pushButton.setObjectName("pushButton")
+
+        #Beginning of image labels (where image will be showed)
+        # label_5 for noise (lower right corner)
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(460, 320, 281, 191))
+        self.label_5.setText("")
+        self.label_5.setOpenExternalLinks(True)
+        self.label_5.setObjectName("label_5")
+        self.label_5.setScaledContents(True)
+        imagePath = second.images[3]
+        pixmap =QPixmap(imagePath)
+        self.label_5.setPixmap(QPixmap(pixmap))
+        
+
+        # original (upper left corner)
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(60, 30, 271, 191))
+        self.label_3.setText("")
+        self.label_3.setObjectName("label_3")
+        self.label_3.setScaledContents(True)
+        imagePath = second.images[0]
+        pixmap =QPixmap(imagePath)
+        self.label_3.setPixmap(QPixmap(pixmap))
+
+        ## Detector : (upper right corner)
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(460, 30, 271, 191))
+        self.label_4.setText("")
+        self.label_4.setObjectName("label_4")
+        self.label_4.setScaledContents(True)
+        imagePath = second.images[2]
+        pixmap =QPixmap(imagePath)
+        self.label_4.setPixmap(QPixmap(pixmap))
+
+
+        #GrayScale (lower left corner)
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(60, 320, 281, 191))
+        self.label_6.setText("")
+        self.label_6.setObjectName("label_6")
+        self.label_6.setScaledContents(True)
+        imagePath = second.images[1]
+        pixmap =QPixmap(imagePath)
+        self.label_6.setPixmap(QPixmap(pixmap))
+        #End of image labels (where image will be showed)
+
+        self.label_7 = QtWidgets.QLabel(self.centralwidget)
+        self.label_7.setGeometry(QtCore.QRect(100, 540, 181, 31))
+
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+
+        self.label_7.setFont(font)
+        self.label_7.setObjectName("label_7")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        pixmap =QPixmap(imagePath)
+        self.label.setPixmap(QPixmap(pixmap))
+
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; color:#1994c8;\">Original Image</span></p></body></html>"))
+        self.label_2.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" color:#1994c8;\">Detector</span></p></body></html>"))
+        self.pushButton.setText(_translate("MainWindow", "Noise reduction"))
+        self.label_7.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; color:#1994c8;\">Gray level Image</span></p></body></html>"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_thirdPageWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
